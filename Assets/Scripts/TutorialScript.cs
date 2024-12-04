0f654cb9f929eb5c4e923d329d6490f382327e26
NoorUlAin-Asghar <noorulainasghar@gmail.com>
2024-12-04 05:49:35 +0500

    Flow of Tutorial

--- Assets/Scripts/TutorialScript.cs
+++ Assets/Scripts/TutorialScript.cs
@@ -7,13 +7,14 @@ using UnityEngine;
 public class TutorialScript : MonoBehaviour
 {
     public GameObject TutorialUI;  // UI for "Score"
-    public GameObject GameManager;  
-
+    public GameObject GameManager;
+    public GameObject Background;
 
     public void loadTutorialScene()
     {
         TutorialUI.SetActive(false);
         GameManager.SetActive(true);
+        Background.SetActive(true);
     }
 
 }
